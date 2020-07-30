dd3795ca070136c16cb52199e0728ed50336c65b
Mauricio <mba097@gmail.com>
2020-07-30 19:40:02 -0300

    Configura login

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -1,41 +1,58 @@
-var GoogleStrategy = require('passport-google-oauth20').Strategy;
-const passport = require('passport')
 const util = require('../util/uteis')
 const authentication = require('../util/authentication')
+const connection = require('../database/connection');
 
 module.exports = {
     async loginCliente(req, res) {
         const email = req.body.email;
         const password = req.body.password;
 
-        // const client = await connection('clients')
-        //     .select("*")
-        //     .where("email", email)
-        //     .first()
+        const client = await connection('clients')
+            .select("*")
+            .where("email", email)
+            .first()
 
-        // if (client == undefined)
-        //     return res.status(401).json({
-        //         msg: "Client do not exist"
-        //     })
+        if (client == undefined)
+            return res.status(401).json({
+                msg: "Cliente não cadastrado"
+            })
 
-        // if (util.descriptografar(client.password) == password)
-        //     return res.status(401).json({
-        //         msg: "Senha incorreta"
-        //     })
+        if (util.descriptografar(client.password) == password)
+            return res.status(401).json({
+                msg: "Senha incorreta"
+            })
 
+        delete client.password
+        
         return res.status(200).json({
-            token: authentication.gerarJWT({ id: "teste" }),
-            // nome:client.name
+            token: authentication.gerarJWT({ cpf: client.cpf }),
         })
     },
 
-
     async loginVendedor(req, res) {
-        req.session = null
-        req.logout()
-        res.redirect('/good')
+        const email = req.body.email;
+        const password = req.body.password;
+
+        const seller = await connection('sellers')
+            .select("*")
+            .where("email", email)
+            .first()
+
+        if (seller == undefined)
+            return res.status(401).json({
+                msg: "Vendedor não existe"
+            })
+
+        if (util.descriptografar(seller.password) == password)
+            return res.status(401).json({
+                msg: "Senha incorreta"
+            })
+
+        delete seller.password
+
+        return res.status(200).json({
+            token: authentication.gerarJWT({ cpf_cnpj: seller.cpf_cnpj }),
+            user: seller,
+        })
     },
-
-
-
 }
