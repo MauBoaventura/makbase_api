d550735348852ca74ab4c917ebf3002526dc5c55
Mauricio <mba097@gmail.com>
2020-08-03 20:25:09 -0300

    Adiciona verificacao da autorização

--- src/controllers/ClientController.js
+++ src/controllers/ClientController.js
@@ -22,7 +22,7 @@ module.exports = {
 
         if (client == undefined)
             return res.status(401).json({
-                error: "Client do not exist"
+                msg: "Client do not exist"
             })
         res.json(client)
     },
@@ -34,14 +34,14 @@ module.exports = {
         //Verifica se o cpf_cnpj já esta sendo utilizado
         if (util.existe_Cliente_cpf_cnpj(cpf_cnpj)) {
             return res.status(401).json({
-                error: "Cpf already used!"
+                msg: "Cpf already used!"
             })
         }
 
         //Verifica se o email já esta sendo utilizado
         if (util.existe_Cliente_email(email)) {
             return res.status(401).json({
-                error: "Email already used!"
+                msg: "Email already used!"
             })
         }
 
@@ -53,48 +53,70 @@ module.exports = {
     },
 
     async delete(req, res) {
+        const cliente_header = req.userId;
+
         const cpf_cnpj = req.params.cpf_cnpj;
 
-        //Verifica se o cpf_cnpj já esta sendo utilizado
-        if (!util.existe_Cliente_cpf_cnpj(cpf_cnpj)) {
+        if (cliente_header == cpf_cnpj) {
+            //Verifica se o cpf_cnpj existe
+            if (!util.existe_Cliente_cpf_cnpj(cpf_cnpj)) {
+                return res.status(401).json({
+                    msg: "Client do not exist!"
+                })
+            }
+
+            await connection('clients')
+                .where({ cpf: cpf })
+                .update("deleted_at", Date.now)
+
+            res.status(204).send()
+
+        } else {
             return res.status(401).json({
-                error: "Client do not exist!"
+                msg: "Access Denied!"
             })
         }
 
-        await connection('clients').where("cpf_cnpj", cpf_cnpj).delete()
-
-        res.status(204).send()
     },
 
     async update(req, res) {
+        const cliente_header = req.userId;
+
         const cpf = req.params.cpf_cnpj;
 
-        //Verifica se o cpf_cnpj já esta sendo utilizado
-        if (!util.existe_Cliente_cpf_cnpj(cpf)) {
+        if (cliente_header == cpf_cnpj) {
+
+            //Verifica se o cpf_cnpj já esta sendo utilizado
+            if (!util.existe_Cliente_cpf_cnpj(cpf)) {
+                return res.status(401).json({
+                    msg: "Client do not exist!"
+                })
+            }
+
+            //Verifica se o cpf_cnpj novo já esta sendo utilizado
+            if (util.existe_Cliente_cpf_cnpj(req.body.cpf_cnpj)) {
+                return res.status(401).json({
+                    msg: "New CPF already used!"
+                })
+            }
+
+            //Verifica se o email novo já esta sendo utilizado
+            if (util.existe_Cliente_email(req.body.email)) {
+                return res.status(401).json({
+                    msg: "New email already used!"
+                })
+            }
+
+            await connection('clients')
+                .where({ cpf: cpf })
+                .update(req.body)
+
+            res.status(200).send()
+        } else {
             return res.status(401).json({
-                error: "Client do not exist!"
+                msg: "Access Denied!"
             })
         }
-        
-        //Verifica se o cpf_cnpj novo já esta sendo utilizado
-        if (util.existe_Cliente_cpf_cnpj(req.body.cpf_cnpj)) {
-            return res.status(401).json({
-                error: "New CPF already used!"
-            })
-        }
 
-        //Verifica se o email novo já esta sendo utilizado
-        if (util.existe_Cliente_email(req.body.email)) {
-            return res.status(401).json({
-                error: "New email already used!"
-            })
-        }      
-
-        let client = await connection('clients')
-            .where({cpf:cpf})
-            .update(req.body)
-
-        res.status(200).send()
     }
 };
