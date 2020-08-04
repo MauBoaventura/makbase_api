0b94da0ef8489b187f2c3d133af84f32b0425cfb
Mauricio <mba097@gmail.com>
2020-08-03 21:32:39 -0300

    Verifica autorizações do vendedor

--- src/controllers/SellerController.js
+++ src/controllers/SellerController.js
@@ -38,13 +38,13 @@ module.exports = {
                 error: "Cpf already used!"
             })
         }
-        
+
         if (await util.existe_Vendedor_email(email)) {
             return res.status(401).json({
                 error: "Cpf already used!"
             })
         }
-        
+
         try {
             //Criptografar senha
             req.body.password = await util.criptografar(req.body.password)
@@ -62,27 +62,77 @@ module.exports = {
     },
 
     async delete(req, res) {
+        const cliente_header = req.userId;
         const cpf_cnpj = req.params.cpf_cnpj;
 
-        const seller = await connection('sellers')
-            .select("*")
-            .where("cpf_cnpj", cpf_cnpj)
-            .first()
+        if (cliente_header == cpf_cnpj) {
 
-        if (seller == undefined)
+            if (!util.existe_Vendedor_cpf_cnpj(cpf_cnpj)) {
+                return res.status(401).json({
+                    msg: "Seller do not exist!"
+                })
+            }
+
+            await connection('sellers')
+                .where("cpf_cnpj", cpf_cnpj)
+                .update("deleted_at", Date.now)
+
+            res.status(204).send()
+        } else {
             return res.status(401).json({
-                error: "Sellers not exist"
+                msg: "Access Denied!"
             })
-
-        await connection('sellers').where("cpf_cnpj", cpf_cnpj).delete()
-
-        res.status(204).send()
+        }
     },
 
     async update(req, res) {
+        const vendedor_header = req.userId;
         const cpf_cnpj = req.params.cpf_cnpj;
-        console.log(cpf_cnpj)
-        
+
+        if (vendedor_header == cpf_cnpj) {
+
+            const client = await connection('clients')
+                .select("*")
+                .where("cpf_cnpj", cpf_cnpj)
+                .first()
+
+            //Verifica se o cpf_cnpj já esta sendo utilizado
+            if (!util.existe_Vendedor_cpf_cnpj(cpf_cnpj)) {
+                return res.status(401).json({
+                    msg: "Vendedor do not exist!"
+                })
+            }
+
+            if (req.body.cpf_cnpj != vendedor_header)
+                //Verifica se o cpf_cnpj novo já esta sendo utilizado
+                if (util.existe_Vendedor_cpf_cnpj(req.body.cpf_cnpj)) {
+                    return res.status(401).json({
+                        msg: "New CPF already used!"
+                    })
+                }
+
+            if (client.email != req.body.email) {
+                //Verifica se o email novo já esta sendo utilizado
+                if (util.existe_Vendedor_email(req.body.email)) {
+                    return res.status(401).json({
+                        msg: "New email already used!"
+                    })
+                }
+            }
+            await connection('sellers')
+                .where({ cpf_cnpj: cpf_cnpj })
+                .update(req.body)
+
+            res.status(200).send()
+        } else {
+            return res.status(401).json({
+                msg: "Access Denied!"
+            })
+        }
+
+
+
+
         //Verifica se o cpf_cnpj já esta sendo utilizado
         if (!util.existe_Vendedor_cpf_cnpj(cpf_cnpj)) {
             return res.status(401).json({
@@ -90,7 +140,7 @@ module.exports = {
             })
         }
         console.log(cpf_cnpj)
-        
+
         //Verifica se o cpf_cnpj novo já esta sendo utilizado
         if (util.existe_Vendedor_cpf_cnpj(req.body.cpf_cnpj)) {
             return res.status(401).json({
@@ -104,11 +154,11 @@ module.exports = {
                 error: "New email already used!"
             })
         }
-        
+
         let seller = await connection('sellers')
-        .where("cpf_cnpj",cpf_cnpj)
-        .update(req.body)
-        
+            .where("cpf_cnpj", cpf_cnpj)
+            .update(req.body)
+
         res.status(200).send();
     }
 };
