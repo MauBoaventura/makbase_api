c1c48bc536e7dd5c129f4e75a7faa426cb3ac74b
Mauricio <mba097@gmail.com>
2020-08-03 21:22:19 -0300

    Validacao da autorizacao cliente

--- src/controllers/ClientController.js
+++ src/controllers/ClientController.js
@@ -17,7 +17,7 @@ module.exports = {
 
         const client = await connection('clients')
             .select("*")
-            .where("cpf_cnpj", cpf_cnpj)
+            .where("cpf", cpf_cnpj)
             .first()
 
         if (client == undefined)
@@ -82,7 +82,12 @@ module.exports = {
         const cliente_header = req.userId;
         const cpf = req.params.cpf_cnpj;
 
-        if (cliente_header == cpf_cnpj) {
+        if (cliente_header == cpf) {
+
+            const client = await connection('clients')
+                .select("*")
+                .where("cpf", cpf)
+                .first()
 
             //Verifica se o cpf_cnpj já esta sendo utilizado
             if (!util.existe_Cliente_cpf_cnpj(cpf)) {
@@ -91,20 +96,22 @@ module.exports = {
                 })
             }
 
-            //Verifica se o cpf_cnpj novo já esta sendo utilizado
-            if (util.existe_Cliente_cpf_cnpj(req.body.cpf_cnpj)) {
-                return res.status(401).json({
-                    msg: "New CPF already used!"
-                })
-            }
+            if (req.body.cpf_cnpj != cliente_header)
+                //Verifica se o cpf_cnpj novo já esta sendo utilizado
+                if (util.existe_Cliente_cpf_cnpj(req.body.cpf_cnpj)) {
+                    return res.status(401).json({
+                        msg: "New CPF already used!"
+                    })
+                }
 
-            //Verifica se o email novo já esta sendo utilizado
-            if (util.existe_Cliente_email(req.body.email)) {
-                return res.status(401).json({
-                    msg: "New email already used!"
-                })
+            if (client.email != req.body.email) {
+                //Verifica se o email novo já esta sendo utilizado
+                if (util.existe_Cliente_email(req.body.email)) {
+                    return res.status(401).json({
+                        msg: "New email already used!"
+                    })
+                }
             }
-
             await connection('clients')
                 .where({ cpf: cpf })
                 .update(req.body)
