9c08f772eb3f1407e65a9964df65b3b4bf7723e9
Mauricio <mboaraujo@gmail.com>
2020-08-07 10:30:09 -0300

    Retirada de campos unicos

--- src/database/migrations/20200526005121_create_client.js
+++ src/database/migrations/20200526005121_create_client.js
@@ -2,11 +2,11 @@ exports.up = function (knex, Promise) {
     return knex.schema.createTable('clients', function (table) {
         table.increments();
         knex.raw('`id` INT NULL AUTO_INCREMENT);');
-        table.string('cpf').notNullable().unique();
+        table.string('cpf').notNullable()//.unique();
         table.binary('avatar');
         table.string('name').notNullable();
         table.string('lastname').notNullable();
-        table.string('email').notNullable().unique();
+        table.string('email').notNullable()//.unique();
         table.datetime('age').notNullable();
         table.string('password').notNullable();
         table.timestamp('created_at').notNullable().defaultTo(knex.raw('CURRENT_TIMESTAMP'));
